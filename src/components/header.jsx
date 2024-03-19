8f72010d7c72f40f97cba52f330485f183b6aee6
WillamNyunt <williamnyunt@outlook.com>
2024-03-19 10:11:42 +0000

    Header component added

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header(props) {
+    return (
+        <header id='header'>
+              <img src='investment-calculator-logo.png' alt='investment calculator logo' />
+            <h1>Investment calculator</h1>
+        </header>
+    )
+}
