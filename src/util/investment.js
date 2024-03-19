7a34100eba0d338d65bd4901528070648f8ccb9f
WillamNyunt <williamnyunt@outlook.com>
2024-03-19 16:24:48 +0000

    Appending investment.js
    Making sure all the money logic is in the JS file.

--- src/util/investment.js
+++ src/util/investment.js
@@ -1,38 +1,59 @@
+// The browser-provided Intl API is used to prepare a formatter object
+// This object offers a "format()" method that can be used to format numbers as currency
+// Example Usage: formatter.format(1000) => yields "$1,000"
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+});
+
+
 // This function expects a JS object as an argument
 // The object should contain the following properties
 // - initialInvestment: The initial investment amount
 // - annualInvestment: The amount invested every year
 // - expectedReturn: The expected (annual) rate of return
 // - duration: The investment duration (time frame)
-export function calculateInvestmentResults({
-  initialInvestment,
-  annualInvestment,
-  expectedReturn,
-  duration,
-}) {
-  const annualData = [];
+export function calculateInvestmentResults({ initialInvestment, annualInvestment, expectedReturn, duration }) {
+  let investmentResults = [];
+  let totalInterest = 0;
+  let yearlyInterest = 0;
+  let investedCapital = initialInvestment;
   let investmentValue = initialInvestment;
 
-  for (let i = 0; i < duration; i++) {
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    annualData.push({
-      year: i + 1, // year identifier
-      interest: interestEarnedInYear, // the amount of interest earned in this year
-      valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
-    });
+  for (let year = 1; year <= duration; year++) {
+    investmentValue = investmentValue * (1 + (expectedReturn/100)) + annualInvestment;
+    investedCapital += annualInvestment
+
+    //if greater than year 
+    if (year > 1) {
+      yearlyInterest = (investmentValue - investedCapital) - totalInterest;
+    } else {
+      yearlyInterest = investmentValue - investedCapital;
+    }
+
+    totalInterest = investmentValue - investedCapital
+
+    investmentResults.push({
+      year,
+      investmentValue,
+      yearlyInterest,
+      totalInterest,
+      investedCapital
+    })
   }
 
-  return annualData;
+  let processedInvestmentResults = [...investmentResults.map(r => ({
+    year: r.year, 
+    investmentValue: formatter.format(r.investmentValue),
+    yearlyInterest: formatter.format(r.yearlyInterest),
+    totalInterest: formatter.format(r.totalInterest),
+    investedCapital: formatter.format(r.investedCapital),
+  }))]
+
+  return processedInvestmentResults;
 }
 
-// The browser-provided Intl API is used to prepare a formatter object
-// This object offers a "format()" method that can be used to format numbers as currency
-// Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 0,
-  maximumFractionDigits: 0,
-});
+
+
